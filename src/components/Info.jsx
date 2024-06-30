e9d045834aa006292a2e9958442217e097014251
ShyneADL <benex2002@gmail.com>
2024-06-30 12:36:39 +0100

    added 1 more section

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -4,12 +4,37 @@ import { item1, item2, item3, arrow, corner } from '../assets'
 const Info = () => {
   return (
     <section className='py-[104px] bg-white flex flex-col items-center gap-[160px] w-full'>
-      <h3 className='font-sora font-700 text-[32px] leading-[40.32px] text-center text-black w-[674px]'>Walletry makes blockchain development accessible for everyone.</h3>
-      <div className='flex items-center gap-[22.25px]'>
-        <div className='p-10 bg-white shadow flex flex-col items-start gap-[15px] relative'>
-          <img src={item1} className='absolute top-[-20px] left-[50%] translate-x-[-50%]'/>
-          <img src={corner} className='absolute right-[0px] top-[0px]' />
-          <h3 className='font-sora font-700 text-[32px] leading-[38px] tracking-[-0.64px]'>Globally-accesible, transformational API</h3>
+      <h3 className='font-sora font-bold text-[32px] leading-[40.32px] text-center text-black w-[674px]'>Walletry makes blockchain development accessible for everyone.</h3>
+      <div className='flex items-center gap-[10px]'>
+        <div className='p-[30px] info_box shadow flex flex-col items-start gap-[15px] relative w-[420px]'>
+          <img src={item1} className='absolute top-[-180px] left-[50%] translate-x-[-50%] w-[340px]'/>
+          <img src={corner} className='absolute right-[8px] top-[8px] w-[16.5px] h-[48px]' />
+          <h3 className='font-sora font-bold text-[32px] leading-[38px] tracking-[-0.64px] mt-[85px]'>Globally-accesible, transformational API</h3>
+          <p className='font-inter font-normal text-[16px] leading-[27px] tracking-[-0.36px] text-darkGrey w-[302px]'>Your data can be transformed, organized, and shared across applications for anyone to query with just a few keystrokes.</p>
+          <div className='flex items-center justify-between mt-[40px] w-full'>
+            <p className='font-sora font-semibold text-[18px] leading-[27px] tracking-[-0.36px] text-primary'>Learn more</p>
+            <img src={arrow} className='w-[26px] h-[26px]'/>
+          </div>
+        </div>
+        <div className='p-[30px] info_box shadow flex flex-col items-start gap-[15px] relative w-[420px]'>
+          <img src={item2} className='absolute top-[-180px] left-[50%] translate-x-[-50%] w-[340px]'/>
+          <img src={corner} className='absolute right-[8px] top-[8px] w-[16.5px] h-[48px]' />
+          <h3 className='font-sora font-bold text-[32px] leading-[38px] tracking-[-0.64px] mt-[85px]'>Globally-accesible, transformational API</h3>
+          <p className='font-inter font-normal text-[16px] leading-[27px] tracking-[-0.36px] text-darkGrey w-[302px]'>Your data can be transformed, organized, and shared across applications for anyone to query with just a few keystrokes.</p>
+          <div className='flex items-center justify-between mt-[40px] w-full'>
+            <p className='font-sora font-semibold text-[18px] leading-[27px] tracking-[-0.36px] text-primary'>Learn more</p>
+            <img src={arrow} className='w-[26px] h-[26px]'/>
+          </div>
+        </div>
+        <div className='p-[30px] info_box shadow flex flex-col items-start gap-[15px] relative w-[420px]'>
+          <img src={item3} className='absolute top-[-180px] left-[50%] translate-x-[-50%] w-[340px]'/>
+          <img src={corner} className='absolute right-[8px] top-[8px] w-[16.5px] h-[48px]' />
+          <h3 className='font-sora font-bold text-[32px] leading-[38px] tracking-[-0.64px] mt-[85px]'>Globally-accesible, transformational API</h3>
+          <p className='font-inter font-normal text-[16px] leading-[27px] tracking-[-0.36px] text-darkGrey w-[302px]'>Your data can be transformed, organized, and shared across applications for anyone to query with just a few keystrokes.</p>
+          <div className='flex items-center justify-between mt-[40px] w-full'>
+            <p className='font-sora font-semibold text-[18px] leading-[27px] tracking-[-0.36px] text-primary'>Learn more</p>
+            <img src={arrow} className='w-[26px] h-[26px]'/>
+          </div>
         </div>
       </div>
     </section>
--- src/components/Onboarding.jsx
+++ src/components/Onboarding.jsx
@@ -2,9 +2,23 @@ import React from 'react'
 
 const Onboarding = () => {
   return (
-    <div>
-      
-    </div>
+    <section className='py-[90px] bg-darkBlue flex flex-col items-center gap-[86px] w-full'>
+      <h3 className='font-sora font-bold text-[32px] leading-[40.32px] text-center text-white w-[883px]'>Walletry makes Life easier for onboarding experience  for everyone.</h3>
+      <div className='flex items-center gap-[108px] mb-[110px]'>
+        <ul className='flex flex-col items-center gap-[26px]'>
+          <li className='font-sora font-semibold text-[24px] leading-[38px] tracking-[-0.64px] text-lightGrey'>Wallet</li>
+          <li className='font-sora font-semibold text-[24px] leading-[38px] tracking-[-0.64px] text-lightGrey'>Gaming</li>
+          <li className='font-sora font-semibold text-[24px] leading-[38px] tracking-[-0.64px] text-lightGrey'>NFT</li>
+          <li className='font-sora font-semibold text-[24px] leading-[38px] tracking-[-0.64px] text-lightGrey'>Social</li>
+          <li className='font-sora font-semibold text-[24px] leading-[38px] tracking-[-0.64px] text-lightGrey'>MintCoin</li>
+        </ul>
+        <div className='flex flex-col items-start gap-[35px] bg-white rounded-[24px] py-[85px] pl-[73px] pr-[155px]'>
+          <h4 className='font-sora font-bold text-[36px] leading-[52px] tracking-[-0.5px] text-black  w-[739px]'>Convenience and conversion with the right amount of security at every stage.</h4>
+          <p className='font-inter font-normal text-[22px] leading-[26.4px] text-darkGrey w-[711.65px]'>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatu</p>
+          <button className='gradient_btn py-[8.5px] px-[39.5px] text-[16px] leading-[28px]'>Let's Go</button>
+        </div>
+      </div>
+    </section>
   )
 }
 
