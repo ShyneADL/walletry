ace2cff8a0b3dd29e25fab9a4a2ba4393e48f276
ShyneADL <benex2002@gmail.com>
2024-06-30 20:57:17 +0100

    adding files

--- src/assets/index.js
+++ src/assets/index.js
@@ -5,6 +5,15 @@ import item1 from './item1.png'
 import item2 from './item2.png'
 import item3 from './item3.png'
 import corner from './corner.png'
+import icon1 from './icon1.svg'
+import icon2 from './icon2.svg'
+import icon3 from './icon3.svg'
+import icon4 from './icon4.svg'
+import icon5 from './icon5.svg'
+import icon6 from './icon6.svg'
+import atom from './atom.png'
+import device from './device.png'
+import faded from './faded.png'
 
 
 export { 
@@ -15,4 +24,13 @@ export {
     item2,
     item3,
     corner,
+    icon1,
+    icon2,
+    icon3,
+    icon4,
+    icon5,
+    icon6,
+    atom,
+    device,
+    faded,
  }
--- src/components/Solutions.jsx
+++ src/components/Solutions.jsx
@@ -1,10 +1,46 @@
 import React from 'react'
+import { icon1, icon2, icon3, icon4, icon5, icon6 } from '../assets'
 
 const Solutions = () => {
   return (
-    <div>
-      
-    </div>
+    <section className='bg-white flex flex-col items-center gap-[58px] py-[120px] px-[185px] w-full'>
+      <div className='flex flex-col items-center gap-[20px]'>
+        <h3 className='font-sora font-bold text-[32px] leading-[40.32px] text-center text-black'>Scaling solutions, amplified</h3>
+        <p className='font-inter font-normal text-[24px] leading-[36px] tracking-[-0.36px] text-center text-darkGrey'>Walletry’s complete suite of blockchain-scaling solutions.</p>
+      </div>
+      <div className='grid_box'>
+        <div id='item-0' className='flex flex-col items-start gap-[15px] rounded-[24px] pl-[32px] pr-[20px] pt-[32px] pb-[66px] shadow_box'>
+          <img src={icon1} className='w-8 h-8' />
+          <h3 className='font-sora font-bold text-[32px] leading-[38px] tracking-[-0.64px] text-black mt-5'>Walletry</h3>
+          <p className='font-inter font-normal text-[16px] leading-[27px] tracking-[-0.36px] w-[277.2px]'>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur ma</p>
+        </div>
+        <div id='item-1' className='flex flex-col items-start gap-[15px] rounded-[24px] pl-[32px] pr-[20px] pt-[32px] pb-[66px] shadow_box'>
+          <img src={icon2} className='w-8 h-8' />
+          <h3 className='font-sora font-bold text-[32px] leading-[38px] tracking-[-0.64px] text-black mt-5'>Walletry</h3>
+          <p className='font-inter font-normal text-[16px] leading-[27px] tracking-[-0.36px] w-[277.2px]'>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur ma</p>
+        </div>
+        <div id='item-2' className='flex flex-col items-start gap-[15px] rounded-[24px] pl-[32px] pr-[20px] pt-[32px] pb-[66px] shadow_box'>
+          <img src={icon3} className='w-8 h-8' />
+          <h3 className='font-sora font-bold text-[32px] leading-[38px] tracking-[-0.64px] text-black mt-5'>Walletry</h3>
+          <p className='font-inter font-normal text-[16px] leading-[27px] tracking-[-0.36px] w-[277.2px]'>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur ma</p>
+        </div>
+        <div id='item-3' className='flex flex-col items-start gap-[15px] rounded-[24px] pl-[32px] pr-[20px] pt-[32px] pb-[66px] shadow_box'>
+          <img src={icon4} className='w-8 h-8' />
+          <h3 className='font-sora font-bold text-[32px] leading-[38px] tracking-[-0.64px] text-black mt-5'>Walletry</h3>
+          <p className='font-inter font-normal text-[16px] leading-[27px] tracking-[-0.36px] w-[277.2px]'>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur ma</p>
+        </div>
+        <div id='item-4' className='flex flex-col items-start gap-[15px] rounded-[24px] pl-[32px] pr-[20px] pt-[32px] pb-[66px] shadow_box'>
+          <img src={icon5} className='w-8 h-8' />
+          <h3 className='font-sora font-bold text-[32px] leading-[38px] tracking-[-0.64px] text-black mt-5'>Walletry</h3>
+          <p className='font-inter font-normal text-[16px] leading-[27px] tracking-[-0.36px] w-[277.2px]'>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur ma</p>
+        </div>
+        <div id='item-5' className='flex flex-col items-start gap-[15px] rounded-[24px] pl-[32px] pr-[20px] pt-[32px] pb-[66px] shadow_box'>
+          <img src={icon6} className='w-8 h-8' />
+          <h3 className='font-sora font-bold text-[32px] leading-[38px] tracking-[-0.64px] text-black mt-5'>Walletry</h3>
+          <p className='font-inter font-normal text-[16px] leading-[27px] tracking-[-0.36px] w-[277.2px]'>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur ma</p>
+        </div>
+      </div>
+    </section>
   )
 }
 
