fb46537a0229e3f74ef4ac808a8b4ba22a104104
ShyneADL <benex2002@gmail.com>
2024-07-02 22:23:08 +0100

    final image changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,7 +10,7 @@ const hero = () => {
         <p className='font-inter font-normal text-[18px] leading-[27px] text-center text-darkGrey mt-[10px] w-[550px]'>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duit. Exercitation veniam consequat sunt</p>
         <button className='gradient_btn py-[21px] px-[82.5px] text-[18px] leading-[22.68px] mt-[25px]'>Book a Demo</button>
       <div className='flex flex-col items-center gap-[17px] mt-[634.9px] cursor-pointer'>
-        <div className='p-4 bounce_top'>
+        <div className='p-4 '>
           <img src={mouse} className='w-[38px] h-[64px]'/>
         </div>
         <p className='font-inter font-bold uppercase text-[12px] leading-[14px] tracking-[0.6px] text-center text-darkGrey'>Scroll to explore</p>
